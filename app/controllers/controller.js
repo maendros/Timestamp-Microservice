d61fdd5d47d13f47b6c8171017dad67097fb7e5c
Paschalis <Valitsianius@hotmail.com>
2016-03-27 15:04:48 +0000

    updating controller.js

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -4,25 +4,23 @@ var moment = require('moment');
 module.exports=function(app){
     
    app.get('/:query', function(req,res) {
-  var myDate;
+  var myDate=req.params.query;
+  myDate = moment(req.params.query, "MMMM DD, YYYY");
   
-  if(/^\d{8,}$/.test(req.params.query)) {
-    myDate = moment(req.params.query, "X");
-  } else {
-    myDate = moment(req.params.query, "MMMM D, YYYY");
-  }
+  if( !myDate.isValid()) {
+       res.json({
+      "unix": null,
+      "normal": null
+    });
+    } 
+
 
   if(myDate.isValid()) {
     res.json({
-      unix: myDate.format("X"),
-      normal: myDate.format("MMMM D, YYYY")
+      "unix": myDate.format("X"),
+      "normal": myDate.format("MMMM D, YYYY")
     });
-  } else {
-    res.json({
-      unix: null,
-      normal: null
-    });
-  }
+}
     
    });
     
